01912e3afb920bed5fa715b036c95c1e30fe2797
SuperSuRaccoon <supersuraccoon@gmail.com>
2016-05-03 16:42:54 +0800

    init
    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# GoTiles
-GoTiles
--- tools/ant/complier.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo "Complier Start..."
-set targetFile=TilesPuzzleHtml5-v1.0.js
-del "%cd%\..\..\project\HTML5\%targetFile%"
-call ant.bat -f "%cd%\..\..\project\HTML5\src\build.xml"
-copy "%cd%\..\..\project\HTML5\%targetFile%" %cd%
-echo "Complier End..."
-Pause
